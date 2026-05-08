90e5175cf1ca664f921900d2c74afa39380e4168
pei <ds1231h@gmail.com>
2026-05-08 13:16:58 +0100

    upload measurement script

--- measure/breakdown_measurement_script.sh
+++ measure/breakdown_measurement_script.sh
@@ -22,4 +22,25 @@ bash ./measure_breakdown_time_job.sh duckdb node-based expr o2 none off off off 
 bash ./measure_breakdown_time_job.sh duckdb node-based expr o3 none off off off off off off off && \
 bash ./measure_breakdown_time_job.sh duckdb node-based expr o1 auto off off off off off off off && \
 bash ./measure_breakdown_time_job.sh duckdb node-based expr o2 auto off off off off off off off && \
-bash ./measure_breakdown_time_job.sh duckdb node-based expr o3 auto off off off off off off off
+bash ./measure_breakdown_time_job.sh duckdb node-based expr o3 auto off off off off off off off && \
+# operator jit none split
+bash ./measure_breakdown_time_job.sh duckdb none operator o1 none off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb none operator o2 none off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb none operator o3 none off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb none operator o1 auto off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb none operator o2 auto off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb none operator o3 auto off off off off off off off && \
+# operator jit relationship-center
+bash ./measure_breakdown_time_job.sh duckdb relationship-center operator o1 none off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb relationship-center operator o2 none off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb relationship-center operator o3 none off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb relationship-center operator o1 auto off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb relationship-center operator o2 auto off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb relationship-center operator o3 auto off off off off off off off && \
+# operator jit node-based
+bash ./measure_breakdown_time_job.sh duckdb node-based operator o1 none off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb node-based operator o2 none off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb node-based operator o3 none off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb node-based operator o1 auto off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb node-based operator o2 auto off off off off off off off && \
+bash ./measure_breakdown_time_job.sh duckdb node-based operator o3 auto off off off off off off off
